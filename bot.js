6866d593a933762410724388cf2e03713d0aabcc
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-10-03 21:37:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,13 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
  
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '3';
+const prefix = '1';
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
 });
  
       client.on('ready', () => {
-              client.user.setActivity("3play | by MrSOKA", {type: 'LISTENING'});
+              client.user.setActivity("1play | by MrSOKA", {type: 'LISTENING'});
      
       });
 
